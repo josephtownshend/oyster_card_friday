9cf821871163a5d4b0c916d1daaf5cfbc518fd33
joseph townshend <joetownshend@mac.com>
2019-06-07 10:28:54 +0100

    added behaviour and tests for touch_in and touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,16 +4,29 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :journey
 
 
   def initialize
     @balance = 0
+    @journey = nil
   end
 
   def top_up(amount)
     fail 'Max balance of #{MAXIMUM_BALANCE} exceeded' if balance + amount > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has a balance of 0' do
+  it 'has a default balance of 0' do
     oyster = Oystercard.new
     expect(subject.balance).to eq(0)
     end
@@ -18,5 +18,27 @@ describe Oystercard do
       expect{ subject.top_up 1}.to raise_error 'Max balance of #{MAXIMUM_BALANCE} exceeded'
     end
   end
+
+  describe '#deduct' do
+    it 'can deduct an amount from the balance' do
+      subject.top_up 5
+      expect{ subject.deduct 5}.to change{ subject.balance}.by -5
+    end
+  end
+
+  describe '#touch_in' do
+    it 'it is in a journey when you touch in' do
+      subject.top_up 5
+      expect{ subject.touch_in}.to change{ subject.journey}.to true
+    end
+  end
+
+  describe '#touch_out' do
+    it 'it leaves the journey when you touch out' do
+      subject.top_up 5
+      expect{ subject.touch_out}.to change{ subject.journey}.to false
+    end
+  end
+
 
 end
